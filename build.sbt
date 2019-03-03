de92b48e9ea1344f98031e56cfd3beba444f6cf2
sarra <sara.zammit.chatti@hotmail.com>
2019-03-03 23:42:58 +0100

    first commit spark streaming file watcher

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "HDFSWatcher"
+
+version := "0.1"
+
+scalaVersion := "2.11.11"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.4.0"
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.4.0"
+libraryDependencies += "org.apache.commons" % "commons-lang3" % "3.8.1"
---
+++ src/main/scala/HDFSFileStream.scala
@@ -0,0 +1,48 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.SparkConf
+
+
+object HDFSFileStream {
+
+  def main(args: Array[String]): Unit = {
+    if (args.length != 2) {
+      println("Please provide 2 parameters <directoryToMonitor> <microbatchtime>")
+      System.exit(1)
+    }
+    val directoryToMonitor = args(0)
+    val microBatchTime = args(1).toInt
+    val sparkConf = new SparkConf().setAppName("HDFSFileStream")
+    val sparkStreamingContext = new StreamingContext(sparkConf, Seconds(microBatchTime))
+
+    //logInfo("Value of microBatchTime " + microBatchTime)
+//    logInfo("DirectoryToMonitor " + directoryToMonitor)
+    println("DirectoryToMonitor " + directoryToMonitor)
+
+    val directoryStream = sparkStreamingContext.textFileStream(directoryToMonitor)
+
+//    logInfo("After starting directoryStream")
+    directoryStream.foreachRDD { fileRdd =>
+      println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+      if (fileRdd.count() != 0)
+        processNewFile(fileRdd)
+    }
+
+    sparkStreamingContext.start()
+    sparkStreamingContext.awaitTermination()
+//    logInfo("Exiting HDFSFileStream.main")
+  }
+
+  def processNewFile(fileRDD: RDD[String]): Unit = {
+    println("*******************************************")
+    println("Entering processNewFile " )
+    println("*******************************************")
+    fileRDD.foreach{ line =>
+      println(line)
+    }
+    println("*******************************************")
+    println("Exiting processNewFile " )
+    println("*******************************************")
+
+  }
+}
